df2534b42a8011ca0789a16995471d30489969a5
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2021-10-02 18:33:18 +0530

    Cu cqkrcm postgre sql server connection avinash vijayvargiya (#7)
    * Added: DB Configs
    
    * Added: Tutorial.java for test
    
    * TEST Heroku
    
    * TEST Heroku
    
    * GH-actions
    
    * Added: UserDetails

---
+++ src/main/java/com/ProjectHub/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.ProjectHub;
+
+import com.ProjectHub.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by Avinash Vijayvargiya on 22-09-2021.
+ */
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
