2e4959392275f6051b1c4af0835f80aff233e76a
avinash14022002 <avinash14022002@gmail.com>
2021-10-10 11:04:32 +0530

    Added: Teacher all projects and Teacher detailed project endpoints in TeacherController.java

--- src/main/java/com/ProjectHub/controller/TeacherController.java
+++ src/main/java/com/ProjectHub/controller/TeacherController.java
@@ -5,12 +5,41 @@ import com.ProjectHub.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Avinash Vijayvargiya on 10-10-2021.
  */
+@RestController
+@RequestMapping("/api")
 public class TeacherController {
 
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping("/teacherProject")
+    public ResponseEntity<List<Project>> getAllProjects(@RequestParam(required = false) Long id) {
+        try {
+            List<Project> tutorials = new ArrayList<>();
+
+            if (id == null)
+                tutorials.addAll(projectRepository.findAll());
+            else
+                tutorials.addAll(projectRepository.getProjectByProjectId(id));
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
