764da41463caf27107c260b86be9fb96488b92b4
Anjali <anjali184a@gmail.com>
2021-12-23 10:41:54 +0530

    Created endpoint to return project details which include student names

--- src/main/java/com/ProjectHub/service/ProjectProfileService.java
+++ src/main/java/com/ProjectHub/service/ProjectProfileService.java
@@ -1,22 +1,31 @@
 package com.ProjectHub.service;
 
 import com.ProjectHub.entities.Project;
+import com.ProjectHub.entities.StudentProfile;
+import com.ProjectHub.model.ProjectDetails;
 import com.ProjectHub.model.ProjectProfileModel;
 import com.ProjectHub.model.ProjectSubmissionModel;
 import com.ProjectHub.repository.ProjectRepository;
+import com.ProjectHub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ProjectProfileService {
     @Autowired
     ProjectRepository projectRepository;
 
-    ProjectProfileModel getProjectCardDetails(Long projectID){
+    @Autowired
+    UserRepository userRepository;
+
+    ProjectProfileModel getProjectCardDetails(Long projectID) {
         ProjectProfileModel projectProfileModel = new ProjectProfileModel();
         Project project = projectRepository.findByProjectId(projectID);
 
-        if(project!=null){
+        if (project != null) {
             projectProfileModel.setProjectID(project.getProjectId());
             projectProfileModel.setProjectDomain(project.getProjectDomain());
             projectProfileModel.setProjectTitle(project.getProjectTitle());
@@ -42,5 +51,49 @@ public class ProjectProfileService {
         project.setProjectTag3(_project.getProjectTag3());
         projectRepository.save(project);
         return project;
+    }
+
+    private Map<String, String> mapStudentDetails(StudentProfile studentProfile) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("id", String.valueOf(studentProfile.getUsername()));
+        map.put("name", studentProfile.getFirstName() + " " + studentProfile.getLastName());
+        return map;
+    }
+
+    public ProjectDetails getProjectDetails(Long id) {
+        Project project = projectRepository.findByProjectId(id);
+        String guideName = "HardCodedName";
+
+        Map<String, String> guideMap = new HashMap<String, String>();
+        guideMap.put("id", String.valueOf(project.getGuideID()));
+        guideMap.put("name", guideName);
+
+        StudentProfile student1 = userRepository.findByUsername(String.valueOf(project.getStudent1id())).get();
+        StudentProfile student2 = userRepository.findByUsername(String.valueOf(project.getStudent2id())).get();
+        StudentProfile student3 = userRepository.findByUsername(String.valueOf(project.getStudent3id())).get();
+        StudentProfile student4 = userRepository.findByUsername(String.valueOf(project.getStudent4id())).get();
+        StudentProfile student5 = project.getStudent5id() != null ? userRepository.findByUsername(String.valueOf(project.getStudent5id())).get() : null;
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("id", null);
+        map.put("name", null);
+
+        ProjectDetails projectDetails = new ProjectDetails(id,
+                project.getProjectTitle(),
+                project.getProjectDomain(),
+                project.getDescription(),
+                project.getProjectTag1(), project.getProjectTag2(), project.getProjectTag3(),
+                project.getImageUrl(), project.getPptUrl(), project.getPaperUrl(),
+                project.getClosed(),
+                project.getProjectInfo(),
+                guideMap,
+                mapStudentDetails(student1),
+                mapStudentDetails(student2),
+                mapStudentDetails(student3),
+                mapStudentDetails(student4),
+                student5 == null ? map : mapStudentDetails(student5),
+                project.getDeadline()
+        );
+        return projectDetails;
     }
 }
