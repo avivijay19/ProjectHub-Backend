3f2b5b119549254b27d36342013ccd8da49dd281
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2021-12-30 14:30:40 +0530

    Added: Endpoints.java (#32)

---
+++ src/main/java/com/ProjectHub/util/Endpoints.java
@@ -0,0 +1,7 @@
+package com.ProjectHub.util;
+
+public class Endpoints {
+    public static final String STUDENT = "/student";
+    public static final String TEACHER = "/teacher";
+    public static final String ADMIN = "/admin";
+}
