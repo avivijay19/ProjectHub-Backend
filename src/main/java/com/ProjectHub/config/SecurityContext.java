6e3df2928cf976b39d9a5bf1e1ce8dcf22637c59
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-01-05 17:11:27 +0530

    Completed: Auth

--- src/main/java/com/ProjectHub/config/SecurityContext.java
+++ src/main/java/com/ProjectHub/config/SecurityContext.java
@@ -51,7 +51,7 @@ public class SecurityContext extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/studentLogin", "/v3/**", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**",
+                .authorizeRequests().antMatchers("/auth/**", "/v3/**", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**",
                         /* Probably not needed */ "/swagger.json"
                 ).permitAll().
                 anyRequest().authenticated().and().
