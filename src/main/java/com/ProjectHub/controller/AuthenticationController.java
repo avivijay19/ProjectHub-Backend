15f1bde72dbab27d70bcc2a39ff9dd92af39493d
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-01-07 18:22:02 +0530

    test

--- src/main/java/com/ProjectHub/controller/AuthenticationController.java
+++ src/main/java/com/ProjectHub/controller/AuthenticationController.java
@@ -62,51 +62,6 @@ public class AuthenticationController {
         return new ResponseEntity<>(response, responseHeaders, HttpStatus.OK);
 
     }
-
-    @PostMapping(value = "/teacherlogin")
-    public ResponseEntity<?> createAuthenticationTokenTeacher(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-        HttpHeaders responseHeaders = new HttpHeaders();
-
-        Authentication authenticate = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-        );
-        final MyUserDetails userDetails = (MyUserDetails) userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtTokenUtil.generateToken(userDetails);
-        AuthenticationResponse response = new AuthenticationResponse(jwt);
-
-        response.setUsername(userDetails.getUsername());
-        List<String> roles = new ArrayList<>();
-        userDetails.getAuthorities().forEach((a) -> roles.add(a.getAuthority()));
-        response.setRoles(roles);
-
-        return new ResponseEntity<>(response, responseHeaders, HttpStatus.OK);
-
-    }
-
-    @PostMapping(value = "/adminlogin")
-    public ResponseEntity<?> createAuthenticationTokenAdmin(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-        HttpHeaders responseHeaders = new HttpHeaders();
-
-        Authentication authenticate = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-        );
-        final MyUserDetails userDetails = (MyUserDetails) userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtTokenUtil.generateToken(userDetails);
-        AuthenticationResponse response = new AuthenticationResponse(jwt);
-
-        response.setUsername(userDetails.getUsername());
-        response.setFirstName(userDetails.getFirstName());
-        response.setFirstName(userDetails.getLastName());
-        List<String> roles = new ArrayList<>();
-        userDetails.getAuthorities().forEach((a) -> roles.add(a.getAuthority()));
-        response.setRoles(roles);
-
-        return new ResponseEntity<>(response, responseHeaders, HttpStatus.OK);
-
-    }
-
     @PostMapping("/changePassword")
     public ResponseEntity<HttpStatus> changePassword(@RequestBody ChangePasswordModel changePasswordModel) {
         try {
