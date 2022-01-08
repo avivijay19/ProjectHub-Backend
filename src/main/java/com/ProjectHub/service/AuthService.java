bc070c65bf4d3a00e03b5b8d2bb456cbbbd9212b
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2022-01-08 22:34:58 +0530

    added: Mail Sending Service in forget Password (#49)
    * added: Mail Sending Service in forget Password
    
    * added: Sent Mail Function

--- src/main/java/com/ProjectHub/service/AuthService.java
+++ src/main/java/com/ProjectHub/service/AuthService.java
@@ -6,6 +6,8 @@ import com.ProjectHub.model.ChangePasswordModel;
 import com.ProjectHub.repository.TeacherRepository;
 import com.ProjectHub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -18,11 +20,15 @@ public class AuthService {
     @Autowired
     TeacherRepository teacherRepository;
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
     private boolean updateStudentPassword(String studentId, String newPassword, String oldPassword) {
         StudentProfile studentProfile = userRepository.findByUsername(studentId).get();
         if (Objects.equals(studentProfile.getPassword(), oldPassword)) {
             studentProfile.setPassword(newPassword);
             userRepository.save(studentProfile);
+            sendEmail(studentProfile.getEmailId(), studentProfile.getFirstName(), studentProfile.getLastName());
             return true;
         }
         return false;
@@ -45,5 +51,14 @@ public class AuthService {
             return updateTeacherPassword(model.getId(), model.getNewPassword(), model.getOldPassword());
         }
         return false;
+    }
+
+    String sendEmail(String emailId, String firstName, String lastName) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(emailId);
+        msg.setSubject("Successfully Changed Password for ProjectHub.!");
+        msg.setText("Hello " + firstName + " " + lastName + ", \nYou Have successfully changed the password..! \nTeam ProjectHub");
+        javaMailSender.send(msg);
+        return "Mail Sent";
     }
 }
