8f2fcfef6f7d35996c63a3526c25cb7494473a33
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2022-01-08 14:00:20 +0530

    Added: Admin entity (#46)
    Co-authored-by: Anjali <anjali184a@gmail.com>

---
+++ src/main/java/com/ProjectHub/entities/AdminProfile.java
@@ -0,0 +1,39 @@
+package com.ProjectHub.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "admin")
+public class AdminProfile {
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "emailId")
+    private String emailId;
+
+    @Column(name = "roles")
+    private String roles;
+}
