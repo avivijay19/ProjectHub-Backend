847ccba741fecd639d44bf5156753d1b08b8f7fb
Anjali <anjali184a@gmail.com>
2022-01-14 15:37:25 +0530

    Changed JWT token duration from 10 to 6 hours

--- src/main/java/com/ProjectHub/security/jwt/JwtGenerator.java
+++ src/main/java/com/ProjectHub/security/jwt/JwtGenerator.java
@@ -18,7 +18,7 @@ public class JwtGenerator {
         return Jwts.builder().
                 setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 6))
                 .signWith(SignatureAlgorithm.HS512, "tolusecret")
                 .setHeaderParam("Type", "JWT")
                 .compact();
